812632de796fead10b2460b7297c5362b03786dd
Ellis Rugazoora <ellisrgrz@gmail.com>
2023-09-25 16:55:58 -0700

    start for business

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        Placeholder_for("Inputs = input1, input2, input3", constraints = inputs need to be validated) <br></br>
+        Placeholder_for("Refresh button") <br></br>
+        Placeholder_for("output = f(inputs)")
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      
     </div>
+    
   );
 }
 
