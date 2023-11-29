4073987f842673e78fd9b9ca2c4d61766df22fed
Ellis Rugazoora <ellisrgrz@gmail.com>
2023-11-29 14:27:33 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,6 +29,8 @@ function App() {
   var element3 = placeholder("input definition", "for cost. allow for configurability");
   var element2 = placeholder("output", "the business in terms of " + element3);
   var element4 = placeholder("input definition", "within resource")
+  var competitive = "cost is lower for comparable";
+  var within_resources_definition = "Is something that I possess or can get access to. Or I can afford only after a guaranteed sale.";
   //this is the place that I actually influence the app***
   //helloo
   function clickme_callback(){
@@ -46,7 +48,11 @@ function App() {
         <p>{element3}</p>
         <p>{element2}</p>
         <p></p>
+        {placeholder("divider", "below is the functional section")}
         
+        <input onClick={function(){console.log("change the definition solution parameters. (state)")}}>Change the solution parameters</input>
+        <p>The solution: a function of {competitive} and {within_resources_definition}</p>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
           
